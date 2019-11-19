07e256cb5d192139469a5293ed799ff745b09871
LiableDuke <43440836+LiableDuke@users.noreply.github.com>
2019-11-19 09:09:03 +0100

    Library, obj, temp deleted. package imported

---
+++ WindWaker/Assets/Player/Scripts/ZeldaController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeldaController : MonoBehaviour
+{
+    [Header("Value's")]
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private int dashDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 move = input.normalized * movementSpeed;
+
+        transform.Translate(new Vector3(move.x, 0, move.y) * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Dash(input));
+        }
+    }
+
+    private IEnumerator Dash(Vector2 input)
+    {
+        for(int i = 0; i < dashDistance; i++)
+        {
+            Vector2 move = input.normalized * dashSpeed;
+            transform.Translate(new Vector3(move.x, 0, move.y) * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
---
+++ WindWaker/Assets/Player/Scripts/ZeldasCameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZeldasCameraController : MonoBehaviour
+{
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private float zoomSpeed;
+    [SerializeField] Transform cameraLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * turnSpeed);
+
+        if(cameraLocation.position.z > -3.5f || cameraLocation.position.z < -10f)
+        {
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            transform.Translate(0, 0, scroll * zoomSpeed, Space.Self);
+        }
+    }
+}
